c7effe09be53c6ab130849d4145b7cb0a51f3083
Cleny <kushirotm@gmail.com>
2024-10-02 23:28:04 +0700

    feat: connect project reference

---
+++ API/DataAccess/Core/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.IRepository;
+using DataAccess.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Core
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureDependencyInjection(this IServiceCollection services)
+        {
+            services.AddScoped<IManagerRepository, ManagerRepository>();
+        }
+    }
+}
---
+++ API/DataAccess/DAO/ManagerDAO.cs
@@ -0,0 +1,30 @@
+﻿using BusinessObject.Core;
+using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+    public class ManagerDAO
+    {
+        public static List<Manager> GetManagers()
+        {
+            var list = new List<Manager>();
+            try
+            {
+                using (var context = new GearShopContext())
+                {
+                    list = context.Managers.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return list;
+        }
+    }
+}
