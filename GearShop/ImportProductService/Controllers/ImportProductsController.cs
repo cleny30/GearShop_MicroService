4244fb7aca2a95fff8cc476c47907eb30f0dd4e8
Cleny <kushirotm@gmail.com>
2024-10-04 14:52:48 +0700

    feat: add ocelot

---
+++ GearShop/ImportProductService/Controllers/ImportProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImportProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportProductsController : ControllerBase
+    {
+    }
+}
--- GearShop/ImportProductService/Program.cs
+++ GearShop/ImportProductService/Program.cs
@@ -9,7 +9,6 @@ builder.Services.ConfigureDependencyInjection();
 // Configure Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
