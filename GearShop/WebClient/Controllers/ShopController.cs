999f4895a4dcad77c5920d86527366104dee496e
Cleny <kushirotm@gmail.com>
2024-10-05 09:48:36 +0700

    feat: add ShopController

---
+++ GearShop/WebClient/Controllers/ShopController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class ShopController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string sortFilter = Request.Query["sort"].ToString();
+            string orderFilter = Request.Query["order"].ToString();
+            string category = Request.Query["category"].ToString();
+            string brand = Request.Query["brand"].ToString();
+            int currentPage = Request.Query["page"].ToString() != "" ? Convert.ToInt32(Request.Query["page"]) : 1; ;
+
+            return View();
+        }
+    }
+}
