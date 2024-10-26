39a6ffe0a93e46acfa76a826238b68220aa13d14
Cleny <kushirotm@gmail.com>
2024-10-26 18:36:12 +0700

    feat: complete load order, waiting for tuan complete show address

--- GearShop/Repository/Repository/CartRepository.cs
+++ GearShop/Repository/Repository/CartRepository.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessObject.DTOS;
 using DataAccess.DAO;
+using ISUZU_NEXT.Server.Core.Extentions;
 using Repository.IRepository;
 
 namespace Repository.Repository
@@ -18,6 +19,35 @@ namespace Repository.Repository
 
         public List<CartModel> GetCartsByUsername(string username)=>cartDAO.GetCartsByUsername(username);
 
+        public List<UserCartData> GetUserCartDatas(string username, List<string> proIds)
+        {
+            List<UserCartData> cartItems = new List<UserCartData>();
+            foreach (var i in proIds)
+            {
+                cartItems.Add(GetCartDataByUserName(username).FirstOrDefault(p => p.Product.ProId == i));
+            }
+            return cartItems;
+        }
+
         public bool UpdateCartData(CartModel _cart)=> cartDAO.Update(_cart);
+
+        private List<UserCartData> GetCartDataByUserName(string username)
+        {
+            var products = ProductDAO.GetProducts();
+            var carts = GetCartsByUsername(username).ToList();
+            List<UserCartData> list = new List<UserCartData>();
+            foreach (CartModel item in carts)
+            {
+                ProductData pData = new ProductData();
+                pData.CopyProperties(products.FirstOrDefault(p => p.ProId == item.ProId));
+                UserCartData cartData = new UserCartData
+                {
+                    CartModel = item,
+                    Product = pData
+                };
+                list.Add(cartData);
+            }
+            return list;
+        }
     }
 }
