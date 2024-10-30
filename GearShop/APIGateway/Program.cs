d16263b712189455466becb7a9495944abb74b79
Nguyen Huu Duy <139975518+UnkeptHaroldd@users.noreply.github.com>
2024-10-30 16:14:55 +0700

    feat: add docker and complete project

--- GearShop/APIGateway/Program.cs
+++ GearShop/APIGateway/Program.cs
@@ -3,7 +3,10 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(80);
+});
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
